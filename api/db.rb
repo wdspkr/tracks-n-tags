c5414967278f6f2791693c753a4374b75a75985a
Timo Recke <timo.recke@bitspire.de>
2016-04-15 10:45:57 +0200

    Add timestamps to track model

--- api/db.rb
+++ api/db.rb
@@ -1,4 +1,6 @@
 require 'sequel'
 require 'pg'
 
+Sequel::Model.plugin :timestamps, update_on_create: true
+
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/tracksntags')
--- api/db/migrations/001_create_tracks.rb
+++ api/db/migrations/001_create_tracks.rb
@@ -2,6 +2,8 @@ Sequel.migration do
   up do
     create_table(:tracks) do
       primary_key :id
+      Timestamp :created_at
+      Timestamp :updated_at
       String :url, :null => false
     end
   end
